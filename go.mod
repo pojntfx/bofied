f0c0945b79c994bcccbab4507f1e14da24fbb99f
Felix Pojtinger <felix@pojtinger.com>
2021-04-14 23:10:06 +0000

    feat: Add minimal TFTP server

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/pojntfx/bofied
 
 go 1.16
 
-require golang.org/x/net v0.0.0-20210414194228-064579744ee0
+require (
+	github.com/pin/tftp v2.1.0+incompatible // indirect
+	golang.org/x/net v0.0.0-20210414194228-064579744ee0
+)
--- main.go
+++ main.go
@@ -1,27 +1,71 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/pin/tftp"
 	"golang.org/x/net/webdav"
 )
 
 func main() {
-	listenAddress := flag.String("listenAddress", "localhost:15256", "Listen address")
+	// Parse flags
+	webDAVListenAddress := flag.String("webDAVListenAddress", "localhost:15256", "Listen address for the WebDAV server")
+	tftpListenAddress := flag.String("tftpListenAddress", ":69", "Listen address for the TFTP server")
 	workingDir := flag.String("workingDir", ".", "Directory to store data in")
 
 	flag.Parse()
 
-	srv := &webdav.Handler{
+	// Create servers
+	webdavSrv := &webdav.Handler{
 		FileSystem: webdav.Dir(*workingDir),
 		LockSystem: webdav.NewMemLS(),
 	}
 
-	http.Handle("/", srv)
+	tftpSrv := tftp.NewServer(
+		func(filename string, rf io.ReaderFrom) error {
+			// Prevent accessing any parent directories
+			if strings.Contains(filename, "..") {
+				msg := errors.New("blocked request by client to access parent directory" + filename)
 
-	if err := http.ListenAndServe(*listenAddress, srv); err != nil {
+				log.Println(msg)
+
+				return msg
+			}
+
+			// Open file to send
+			fullFilename := filepath.Join(*workingDir, filename)
+			file, err := os.Open(fullFilename)
+			if err != nil {
+				log.Println("could not open file", err)
+
+				return err
+			}
+
+			// Send the file to the client
+			n, err := rf.ReadFrom(file)
+			log.Printf("sent %v (%v bytes)", fullFilename, n)
+
+			return err
+		},
+		nil,
+	)
+
+	// Start servers
+	http.Handle("/", webdavSrv)
+	go func() {
+		if err := http.ListenAndServe(*webDAVListenAddress, webdavSrv); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	if err := tftpSrv.ListenAndServe(*tftpListenAddress); err != nil {
 		log.Fatal(err)
 	}
 }
