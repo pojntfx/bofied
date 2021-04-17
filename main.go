c4821f1731db0359a8ecffc50607c56b2dba54ec
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-04-17 19:40:02 +0200

    docs: Add basic docs to DHCP implementation, start implementation of proxyDHCP server

--- main.go
+++ main.go
@@ -27,14 +27,19 @@ func main() {
 	webDAVListenAddress := flag.String("webDAVListenAddress", "localhost:15256", "Listen address for the WebDAV server")
 	httpListenAddress := flag.String("httpListenAddress", ":15257", "Listen address for the HTTP server")
 	dhcpListenAddress := flag.String("dhcpListenAddress", ":67", "Listen address for the DHCP server")
+	proxyDHCPListenAddress := flag.String("proxyDHCPListenAddress", ":4011", "Listen address for the proxyDHCP server")
 	tftpListenAddress := flag.String("tftpListenAddress", ":69", "Listen address for the TFTP server")
 	workingDir := flag.String("workingDir", ".", "Directory to store data in")
-	advertisedIPFlag := flag.String("advertisedIP", "100.64.154.246", "IP address to advertise in proxyDHCP")
+	advertisedIPFlag := flag.String("advertisedIP", "100.64.154.246", "IP address to advertise in DHCP and proxyDHCP")
 
 	flag.Parse()
 
 	// Process flags
 	advertisedIP := net.ParseIP(*advertisedIPFlag).To4()
+	broadcastAddress, err := net.ResolveUDPAddr("udp", "255.255.255.255:68")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Create servers
 	webdavSrv := &webdav.Handler{
@@ -108,6 +113,7 @@ func main() {
 			}
 
 			go func(r *net.UDPAddr, rawPacket []byte) {
+				// Decode incoming packet
 				packet := gopacket.NewPacket(rawPacket, layers.LayerTypeDHCPv4, gopacket.Default)
 
 				dhcpLayer := packet.Layer(layers.LayerTypeDHCPv4)
@@ -122,6 +128,7 @@ func main() {
 					log.Fatal("received invalid UDP layer")
 				}
 
+				// Process DHCP options
 				isPXE := false
 				clientIdentifierOpt := layers.DHCPOption{}
 				if dhcpPacket.Operation == layers.DHCPOpRequest {
@@ -138,109 +145,173 @@ func main() {
 					}
 				}
 
-				if isPXE {
-					description := "Boot iPXE (BIOS)"
-					vendorOptions := []byte{}
-
-					subOptions := []layers.DHCPOption{
-						layers.NewDHCPOption(
-							6,                        // Option 43 Suboption: (6) PXE discovery control
-							[]byte{byte(0x00000003)}, // discovery control: 0x03, Disable Broadcast, Disable Multicast
-						),
-						layers.NewDHCPOption(
-							10, // Option 43 Suboption: (10) PXE menu prompt
-							append( // menu prompt: 00505845
-								[]byte{
-									0x00, // Timeout: 0
-								},
-								[]byte("PXE")..., // Prompt: PXE
-							),
-						),
-						layers.NewDHCPOption(
-							8, // Option 43 Suboption: (8) PXE boot servers
-							append( // boot servers: 80000164409af2
-								[]byte{
-									0x80, 0x00, // Type: Unknown (32768)
-									0x01, // IP count: 1
-								},
-								advertisedIP..., // IP: 100.64.154.246
-							),
-						),
-						layers.NewDHCPOption(
-							9, // Option 43 Suboption: (9) PXE boot menu
-							append(
-								[]byte{
-									0x80, 0x00, // Type: Unknown (32768)
-									byte(len(description)), // Length: 16
-								},
-								[]byte(description)..., // Description: Boot iPXE (BIOS)
-							),
-						),
-					}
-
-					for _, subOption := range subOptions {
-						vendorOptions = append(
-							vendorOptions,
-							append(
-								[]byte{
-									byte(subOption.Type),
-									subOption.Length,
-								},
-								subOption.Data...,
-							)...,
-						)
-					}
-
-					outBuf := gopacket.NewSerializeBuffer()
-					gopacket.SerializeLayers(
-						outBuf,
-						gopacket.SerializeOptions{
-							FixLengths: true,
-						},
-						&layers.DHCPv4{
-							Operation:    layers.DHCPOpReply,
-							HardwareType: layers.LinkTypeEthernet,
-							HardwareLen:  0,
-							ClientIP:     r.IP,
-							YourClientIP: r.IP,
-							NextServerIP: r.IP,
-							RelayAgentIP: r.IP,
-							Xid:          dhcpPacket.Xid,
-							ClientHWAddr: dhcpPacket.ClientHWAddr,
-							Options: layers.DHCPOptions{
-								layers.NewDHCPOption(
-									layers.DHCPOptMessageType,
-									[]byte{byte(layers.DHCPMsgTypeOffer)},
-								),
-								layers.NewDHCPOption(
-									layers.DHCPOptServerID,
-									advertisedIP,
-								),
-								layers.NewDHCPOption(
-									layers.DHCPOptClassID,
-									[]byte("PXEClient"),
-								),
-								clientIdentifierOpt,
-								layers.NewDHCPOption(
-									layers.DHCPOptVendorOption,
-									vendorOptions,
-								),
-							},
-						},
-					)
-
-					outAddr, err := net.ResolveUDPAddr("udp", "255.255.255.255:68")
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					n, err := conn.WriteToUDP(outBuf.Bytes(), outAddr)
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					log.Printf(`sent %v bytes to client "%v"`, n, outAddr)
+				// Ignore non-PXE packets
+				if !isPXE {
+					return
 				}
+
+				// Create DHCP suboptions
+				bootMenuDescription := "Boot iPXE (BIOS)"
+				subOptions := []layers.DHCPOption{
+					layers.NewDHCPOption(
+						6,                        // Option 43 Suboption: (6) PXE discovery control
+						[]byte{byte(0x00000003)}, // discovery control: 0x03, Disable Broadcast, Disable Multicast
+					),
+					layers.NewDHCPOption(
+						10, // Option 43 Suboption: (10) PXE menu prompt
+						append( // menu prompt: 00505845
+							[]byte{
+								0x00, // Timeout: 0
+							},
+							[]byte("PXE")..., // Prompt: PXE
+						),
+					),
+					layers.NewDHCPOption(
+						8, // Option 43 Suboption: (8) PXE boot servers
+						append( // boot servers: 80000164409af2
+							[]byte{
+								0x80, 0x00, // Type: Unknown (32768)
+								0x01, // IP count: 1
+							},
+							advertisedIP..., // IP: 100.64.154.246
+						),
+					),
+					layers.NewDHCPOption(
+						9, // Option 43 Suboption: (9) PXE boot menu
+						append(
+							[]byte{
+								0x80, 0x00, // Type: Unknown (32768)
+								byte(len(bootMenuDescription)), // Length: 16
+							},
+							[]byte(bootMenuDescription)..., // Description: Boot iPXE (BIOS)
+						),
+					),
+				}
+
+				// Serialize suboptions
+				serializedSubOptions := []byte{}
+				for _, subOption := range subOptions {
+					serializedSubOptions = append(
+						serializedSubOptions,
+						append(
+							[]byte{
+								byte(subOption.Type),
+								subOption.Length,
+							},
+							subOption.Data...,
+						)...,
+					)
+				}
+
+				// Serialize the outgoing packet
+				outBuf := gopacket.NewSerializeBuffer()
+				gopacket.SerializeLayers(
+					outBuf,
+					gopacket.SerializeOptions{
+						FixLengths: true,
+					},
+					&layers.DHCPv4{
+						Operation:    layers.DHCPOpReply,
+						HardwareType: layers.LinkTypeEthernet,
+						HardwareLen:  0,
+						ClientIP:     r.IP,
+						YourClientIP: r.IP,
+						NextServerIP: r.IP,
+						RelayAgentIP: r.IP,
+						Xid:          dhcpPacket.Xid,
+						ClientHWAddr: dhcpPacket.ClientHWAddr,
+						Options: layers.DHCPOptions{
+							layers.NewDHCPOption(
+								layers.DHCPOptMessageType,
+								[]byte{byte(layers.DHCPMsgTypeOffer)},
+							),
+							layers.NewDHCPOption(
+								layers.DHCPOptServerID,
+								advertisedIP,
+							),
+							layers.NewDHCPOption(
+								layers.DHCPOptClassID,
+								[]byte("PXEClient"),
+							),
+							clientIdentifierOpt,
+							layers.NewDHCPOption(
+								layers.DHCPOptVendorOption,
+								serializedSubOptions,
+							),
+						},
+					},
+				)
+
+				// Broadcast the packet
+				n, err := conn.WriteToUDP(outBuf.Bytes(), broadcastAddress)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				log.Printf(`sent %v bytes of DHCP packets to client "%v"`, n, broadcastAddress)
+			}(raddr, buf[:length])
+		}
+	}()
+
+	go func() {
+		// Create the server
+		laddr, err := net.ResolveUDPAddr("udp", *proxyDHCPListenAddress)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		conn, err := net.ListenUDP("udp", laddr)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Read UDP datagrams
+		for {
+			buf := make([]byte, 1024)
+			length, raddr, err := conn.ReadFromUDP(buf)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			go func(r *net.UDPAddr, rawPacket []byte) {
+				// Decode incoming packet
+				packet := gopacket.NewPacket(rawPacket, layers.LayerTypeDHCPv4, gopacket.Default)
+
+				dhcpLayer := packet.Layer(layers.LayerTypeDHCPv4)
+				if dhcpLayer == nil {
+					log.Fatal("received a non-DHCP layer")
+
+					return
+				}
+
+				dhcpPacket, ok := dhcpLayer.(*layers.DHCPv4)
+				if !ok {
+					log.Fatal("received invalid UDP layer")
+				}
+
+				// Process DHCP options
+				isPXE := false
+				clientIdentifierOpt := layers.DHCPOption{}
+				if dhcpPacket.Operation == layers.DHCPOpRequest {
+					for _, option := range dhcpPacket.Options {
+						switch option.Type {
+						case layers.DHCPOptClassID:
+							isPXE, _, _, err = parsePXEClassIdentifier(string(option.Data))
+							if err != nil {
+								log.Fatal(err)
+							}
+						case DHCPOptUUIDGUIDClientIdentifier:
+							clientIdentifierOpt = option
+						}
+					}
+				}
+
+				// Ignore non-PXE packets
+				if !isPXE {
+					return
+				}
+
+				log.Println(clientIdentifierOpt, r, dhcpPacket)
 			}(raddr, buf[:length])
 		}
 	}()
