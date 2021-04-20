1dcd7d5dd00b59f1d71fb6304f99a2e5ba705b12
Felix Pojtinger <felix@pojtinger.com>
2021-04-20 18:59:22 +0200

    feat: Start implementation of file selection state in file explorer

--- pkg/components/file_explorer.go
+++ pkg/components/file_explorer.go
@@ -33,45 +33,90 @@ type FileExplorer struct {
 	IgnoreFileExplorerError  func()
 
 	newCurrentPath string
+	selectedPath   string
 }
 
 func (c *FileExplorer) Render() app.UI {
 	return app.Div().
 		Body(
-			app.H2().
-				Text("Files"),
-			app.Div().
-				Body(
-					app.Div().Body(
-						app.Code().
-							Text(c.CurrentPath),
-					),
-					app.Div().Body(
-						app.Input().
-							Type("text").
-							OnInput(func(ctx app.Context, e app.Event) {
-								c.newCurrentPath = ctx.JSSrc.Get("value").String()
+			app.Div().Body(
+				app.Div().
+					Body(
+						app.H2().
+							Text("Files"),
+						app.Div().
+							Body(
+								app.Code().
+									Text(c.CurrentPath),
+							),
+						app.Div().Body(
+							app.Input().
+								Type("text").
+								OnInput(func(ctx app.Context, e app.Event) {
+									c.newCurrentPath = ctx.JSSrc.Get("value").String()
 
-								c.Update()
-							}),
+									c.Update()
+								}),
+							app.Button().
+								OnClick(func(ctx app.Context, e app.Event) {
+									c.SetCurrentPath(c.newCurrentPath)
+								}).
+								Text("Navigate"),
+						),
+					),
+				app.Div().
+					Body(
 						app.Button().
 							OnClick(func(ctx app.Context, e app.Event) {
-								c.SetCurrentPath(c.newCurrentPath)
+								c.RefreshIndex()
 							}).
-							Text("Navigate"),
+							Text("Refresh"),
+						app.If(
+							c.selectedPath != "",
+							app.Button().
+								OnClick(func(ctx app.Context, e app.Event) {
+									c.SharePath(c.selectedPath)
+								}).
+								Text("Share"),
+							app.If(
+								c.ShareLink != "",
+								app.Div().
+									Body(
+										app.A().
+											Target("_blank").
+											Href(
+												c.ShareLink,
+											),
+									),
+							),
+						),
 					),
-				),
+			),
 			app.Div().
 				Body(
 					app.Ul().
 						Body(
 							app.Range(c.Index).Slice(func(i int) app.UI {
 								return app.Li().
+									OnClick(func(ctx app.Context, e app.Event) {
+										newSelectedPath := filepath.Join(c.CurrentPath, c.Index[i].Name())
+										if c.selectedPath == newSelectedPath {
+											newSelectedPath = ""
+										}
+
+										c.selectedPath = newSelectedPath
+
+										c.Update()
+									}).
 									Body(
 										app.If(
 											c.Index[i].IsDir(),
 											app.Button().
-												OnClick(func(ctx app.Context, e app.Event) {
+												OnDblClick(func(ctx app.Context, e app.Event) {
+													c.selectedPath = ""
+
+													c.Update()
+
 													c.SetCurrentPath(filepath.Join(c.CurrentPath, c.Index[i].Name()))
 												}).
 												Text(c.Index[i].Name()+"/"),
