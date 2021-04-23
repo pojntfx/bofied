699dfedc63dc999f3a66d6f3a1e4c4bf3d07d8d6
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-04-24 00:53:20 +0200

    refactor: Decompose copyable input

---
+++ pkg/components/copyable_input.go
@@ -0,0 +1,37 @@
+package components
+
+import "github.com/maxence-charriere/go-app/v9/pkg/app"
+
+type CopyableInput struct {
+	app.Compo
+
+	Component app.UI
+	ID        string
+}
+
+func (c *CopyableInput) Render() app.UI {
+	return app.Div().
+		Class("pf-c-clipboard-copy").
+		Body(
+			app.Div().
+				Class("pf-c-clipboard-copy__group").
+				Body(
+					c.Component,
+					app.Button().
+						Class("pf-c-button pf-m-control").
+						Type("button").
+						Aria("label", "Copy to clipboard").
+						Aria("labelledby", c.ID).
+						OnClick(func(ctx app.Context, e app.Event) {
+							app.Window().JSValue().Get("document").Call("getElementById", c.ID).Call("select")
+
+							app.Window().JSValue().Get("document").Call("execCommand", "copy")
+						}).
+						Body(
+							app.I().
+								Class("fas fa-copy").
+								Aria("hidden", true),
+						),
+				),
+		)
+}
