8e38b50113293ebbf25ebc75ffb555d0958eb44d
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-04-25 00:43:37 +0200

    refactor: Decompose path picker toolbar

--- pkg/components/file_explorer.go
+++ pkg/components/file_explorer.go
@@ -903,114 +903,24 @@ func (c *FileExplorer) Render() app.UI {
 
 				Title: `Move "` + path.Base(c.selectedPath) + `"`,
 				Body: []app.UI{
-					app.Div().
-						Class("pf-c-toolbar pf-u-pt-0").
-						Body(
-							app.Div().
-								Class("pf-c-toolbar__content").
-								Body(
-									app.Div().
-										Class("pf-c-toolbar__content-section").
-										Body(
-											app.Div().
-												Class("pf-c-toolbar__item pf-m-overflow-menu").
-												Body(
-													app.Div().
-														Class("pf-c-overflow-menu").
-														Body(
-															app.Div().
-																Class("pf-c-overflow-menu__content").
-																Body(
-																	app.Div().
-																		Class("pf-c-overflow-menu__group pf-m-button-group").
-																		Body(
-																			app.Div().
-																				Class("pf-c-overflow-menu__item").
-																				Body(
-																					&Breadcrumbs{
-																						PathComponents: operationPathComponents,
+					&PathPickerToolbar{
+						Index:        c.OperationIndex,
+						RefreshIndex: c.RefreshIndex,
 
-																						CurrentPath:    c.OperationCurrentPath,
-																						SetCurrentPath: c.OperationSetCurrentPath,
+						PathComponents: operationPathComponents,
 
-																						SelectedPath: c.operationSelectedPath,
-																						SetSelectedPath: func(s string) {
-																							c.operationSelectedPath = s
-																						},
-																					},
-																				),
-																		),
-																),
-														),
-												),
-											app.Div().
-												Class("pf-c-toolbar__item pf-m-pagination").
-												Body(
-													app.Div().
-														Class("pf-c-pagination pf-m-compact").
-														Body(
-															app.Div().
-																Class("pf-c-pagination pf-m-compact pf-m-compact").
-																Body(
-																	app.Div().
-																		Class("pf-c-overflow-menu").
-																		Body(
-																			app.Div().
-																				Class("pf-c-overflow-menu__content").
-																				Body(
-																					app.Div().
-																						Class("pf-c-overflow-menu__group pf-m-button-group").
-																						Body(
-																							app.Div().
-																								Class("pf-c-overflow-menu__item").
-																								Body(
-																									app.Button().
-																										Type("button").
-																										Aria("label", "Create directory").
-																										Title("Create directory").
-																										Class("pf-c-button pf-m-plain").
-																										OnClick(func(ctx app.Context, e app.Event) {
-																											c.createDirectoryModalOpen = true
-																										}).
-																										Body(
-																											app.I().
-																												Class("fas fa-folder-plus").
-																												Aria("hidden", true),
-																										),
-																								),
-																						),
-																				),
-																			app.Div().
-																				Class("pf-c-divider pf-m-vertical pf-m-inset-md").
-																				Aria("role", "separator"),
-																			app.Div().
-																				Class("pf-c-overflow-menu__group pf-m-button-group").
-																				Body(
-																					app.Div().
-																						Class("pf-c-overflow-menu__item").
-																						Body(
-																							app.Button().
-																								Type("button").
-																								Aria("label", "Refresh").
-																								Title("Refresh").
-																								Class("pf-c-button pf-m-plain").
-																								OnClick(func(ctx app.Context, e app.Event) {
-																									c.RefreshIndex()
-																								}).
-																								Body(
-																									app.I().
-																										Class("fas fas fa-sync").
-																										Aria("hidden", true),
-																								),
-																						),
-																				),
-																		),
-																),
-														),
-												),
-										),
-								),
-						),
+						CurrentPath:    c.OperationCurrentPath,
+						SetCurrentPath: c.OperationSetCurrentPath,
+
+						SelectedPath: c.operationSelectedPath,
+						SetSelectedPath: func(s string) {
+							c.operationSelectedPath = s
+						},
+
+						OpenCreateDirectoryModal: func() {
+							c.createDirectoryModalOpen = true
+						},
+					},
 					app.If(
 						len(c.OperationIndex) > 0,
 						&FileGrid{
